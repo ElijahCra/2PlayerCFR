4f3e8bb57eeadd5d50c3718bfe4f22072bccffdd
elijahcrain <elijah.b.crain@gmail.com>
2023-11-16 13:57:42 -0500

    geeb11

--- Cards/Cards.hpp
+++ Cards/Cards.hpp
@@ -5,10 +5,21 @@
 #ifndef CARDS_HPP
 #define CARDS_HPP
 
+#include "./HandAbstraction/hand_index.h"
+#include <variant>
+#include <vector>
 
 
 class Cards {
+Cards(int card1, int card2);
+Cards(int card1, int card2, int card3);
 
+public:
+    static hand_index_t plHandtoIndex();
+    static std::vector<int> indexToCards();
+
+    std::array<std::variant<int,hand_index_t>,5> publicHand;
+    std::vector<std::variant<int,hand_index_t>> Hand;
 };
 
 
