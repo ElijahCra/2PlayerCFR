515d2b9996d29f3c823d6efc47d330466a3f20a1
RagingKayak <elijah.b.crain@gmail.com>
2023-11-15 19:54:35 -0500

    yum

---
+++ Cards/Cards.cpp
@@ -0,0 +1,5 @@
+//
+// Created by elijah on 11/15/2023.
+//
+
+#include "Cards.hpp"
---
+++ Cards/Cards.hpp
@@ -0,0 +1,16 @@
+//
+// Created by elijah on 11/15/2023.
+//
+
+#ifndef CARDS_HPP
+#define CARDS_HPP
+
+
+
+class Cards {
+
+};
+
+
+
+#endif //CARDS_HPP
