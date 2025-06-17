c40bb6337ecf7a676d5a0b670a3eb551fa0ec91c
elijah <elijah.b.crain@gmail.com>
2025-06-17 15:24:48 -0400

    timing

--- main.cpp
+++ main.cpp
@@ -5,5 +5,11 @@
 
 int main() {
     CFR::RegretMinimizer<Preflop::Game> Minimize{(std::random_device()())};
+    auto start = std::chrono::high_resolution_clock::now();
     Minimize.Train(1010000);
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+    std::cout << "\nTraining completed in " << duration.count() << " ms\n";
 }
