e148f679ce0ec8cb07b40bc12a7e61721450fdd2
elijah <elijah.b.crain@gmail.com>
2025-07-01 17:39:52 -0400

    single threaded rocks/lru

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ template<typename K, typename V> using MyMap = std::unordered_map<K, V>;
 template<typename T> using MyList = std::list<T>;
 
 int main() {
-    //CFR::RegretMinimizer<Preflop::Game, CFR::HybridNodeStorage<CFR::LRUNodeCache<MyMap,LRUList>>> Minimize; //Rocksdb and lru cache
+    CFR::RegretMinimizer<Preflop::Game, CFR::HybridNodeStorage<CFR::LRUNodeCache<MyMap,LRUList>>> Minimize; //Rocksdb and lru cache
     
-    CFR::RegretMinimizer<Preflop::Game> Minimize; //Raw mem cache
+    //CFR::RegretMinimizer<Preflop::Game> Minimize; //Raw mem cache
     auto start = std::chrono::high_resolution_clock::now();
     Minimize.Train(1010000);
     auto end = std::chrono::high_resolution_clock::now();
