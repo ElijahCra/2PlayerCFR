b0625dde16089bdb54dc7eec2534ed165fb8eac1
elijah <elijah.b.crain@gmail.com>
2025-07-02 14:31:52 -0400

    changes for cfrapp

--- Storage/MapNodeStorage.hpp
+++ Storage/MapNodeStorage.hpp
@@ -29,35 +29,6 @@ private:
     std::unordered_map<std::string, std::shared_ptr<Node>> m_nodeMap;
 };
 
-
-
-std::shared_ptr<Node> MapNodeStorage::getNode(const std::string& infoSet) {
-    auto it = m_nodeMap.find(infoSet);
-    return (it != m_nodeMap.end()) ? it->second : nullptr;
-}
-
-void MapNodeStorage::putNode(const std::string& infoSet, std::shared_ptr<Node> node) {
-    m_nodeMap[infoSet] = node;
-}
-
-bool MapNodeStorage::hasNode(const std::string& infoSet) const {
-    return m_nodeMap.find(infoSet) != m_nodeMap.end();
-}
-
-void MapNodeStorage::removeNode(const std::string& infoSet) {
-    m_nodeMap.erase(infoSet);
-}
-
-size_t MapNodeStorage::size() const {
-    return m_nodeMap.size();
-}
-
-void MapNodeStorage::clear() {
-    m_nodeMap.clear();
-}
-
-
-
 } // namespace CFR
 
 #endif //MAPNODESTORAGE_HPP
