c99d5cd09d99a2685d97e53b3f89fa3568efd86b
elijahcrain <elijah.b.crain@gmail.com>
2025-07-05 01:04:05 -0400

    close but gamestate getting corrupted

--- CFR/MultiThreadedTrainer.hpp
+++ CFR/MultiThreadedTrainer.hpp
@@ -31,9 +31,9 @@ public:
                   << iterationsPerThread << " iterations per thread\n";
 
         for (uint32_t i = 0; i < m_numThreads; ++i) {
-            m_regretMinimizers.emplace_back(std::random_device()(), m_storage);
+            m_regretMinimizers.emplace_back(std::make_unique<RegretMinimizer<GameType, StorageType>>(std::random_device()(), m_storage));
             m_threads.emplace_back(
-                &RegretMinimizer<GameType,StorageType>::Train, &m_regretMinimizers[i],iterationsPerThread
+                &RegretMinimizer<GameType,StorageType>::Train, m_regretMinimizers[i].get(),iterationsPerThread
             );
         }
 
@@ -46,7 +46,7 @@ public:
 private:
     std::shared_ptr<StorageType> m_storage;
     std::vector<std::thread> m_threads;
-    std::vector<RegretMinimizer<GameType, StorageType>> m_regretMinimizers;
+    std::vector<std::unique_ptr<RegretMinimizer<GameType, StorageType>>> m_regretMinimizers;
     uint32_t m_numThreads;
 };
 }
--- CFR/RegretMinimizer.hpp
+++ CFR/RegretMinimizer.hpp
@@ -31,8 +31,8 @@ class RegretMinimizer {
   
   RegretMinimizer(const RegretMinimizer& other) = delete;
   auto operator=(const RegretMinimizer& other) -> RegretMinimizer& = delete;
-  RegretMinimizer(RegretMinimizer&& other) = default;
-  auto operator=(RegretMinimizer&& other) -> RegretMinimizer& = default;
+  RegretMinimizer(RegretMinimizer&& other) = delete;
+  auto operator=(RegretMinimizer&& other) -> RegretMinimizer& = delete;
 
   /// @brief calls cfr algorithm for full game tree (or sampled based on version) traversal the specified number of times
   void Train(uint32_t iterations);
@@ -226,7 +226,7 @@ auto RegretMinimizer<GameType, StorageType>::ExternalSamplingCFR(const GameType 
 
     } else { //sample single player action for non update player
       GameType gamePlusOneAction(game);
-      std::discrete_distribution<int> actionSpread(currentStrategy.begin(),currentStrategy.end());
+      std::discrete_distribution actionSpread(currentStrategy.begin(),currentStrategy.end());
       auto sampledAction = actionSpread(rng);
       gamePlusOneAction.transition(actions[sampledAction]);
       nodeValue = ExternalSamplingCFR(gamePlusOneAction, updatePlayer, probCounterFactual, probUpdatePlayer);
